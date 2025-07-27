07fc73015428ea73114ff459f7ad68013a5c1aef
Daniel Castellanos <danicage.13@outlook.com>
2025-07-27 12:08:49 -0500

    added documentetion for endpoints

--- init-server/backend/app/api/v1/routes/health.py
+++ init-server/backend/app/api/v1/routes/health.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from app.db.session import SessionLocal
@@ -7,12 +9,44 @@ from app.exceptions.custom_exceptions import DatabaseConnectionException
 # Crear un router para las rutas de testeo de salud
 router = APIRouter(tags=["Health"], prefix="/health")
 # Endpoint para verificar la conexión a la base de datos
-@router.get("/db")
+@router.get(
+    "/db", summary="Check Database Connection",
+    description="Verifica la conexión a la base de datos y devuelve un mensaje de éxito o error.",
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Conexión a la base de datos exitosa",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "status": "ok",
+                        "message": "Conexión a la base de datos exitosa",
+                        "data": None
+                    }
+                }
+            }
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Error al conectar con la base de datos",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "status": "error",
+                        "message": "Error al conectar con la base de datos",
+                        "data": None
+                    }
+                }
+            }
+        }
+    }
+)
 def check_database():
     try:
         db = SessionLocal()
         db.execute(text("SELECT 1"))
-        return APIResponse(status="ok", message="Conexión a la base de datos exitosa")
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=jsonable_encoder(APIResponse(status="ok", message="Conexión a la base de datos exitosa"))
+        )
     except SQLAlchemyError as e:
         raise DatabaseConnectionException() from e
     finally:
--- init-server/backend/app/api/v1/routes/user.py
+++ init-server/backend/app/api/v1/routes/user.py
@@ -1,25 +1,153 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from app.utils.response import APIResponse
-from app.exceptions.custom_exceptions import NotFoundException
+from app.exceptions.custom_exceptions import NotFoundException, ConflictException
 from app.crud import user as user_crud
 from sqlalchemy.orm import Session
 from app.db.session import get_db
+from app.schemas.user import UserCreate
 # Crear un router para las rutas de usuarios
 router = APIRouter(tags=["Users"], prefix="/users")
+# Documentacion para los endpoints
+responses = {
+  "get_users": {
+    status.HTTP_200_OK: {
+      "description": "Usuarios encontrados",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "success",
+            "message": "Usuarios encontrados",
+            "data": [
+              {
+                "username": "user1",
+                "id": 1,
+                "created_at": "2025-07-26T18:27:09"
+              }
+            ]
+          }
+        }
+      }
+    },
+    status.HTTP_204_NO_CONTENT: {
+      "description": "No se encontraron usuarios",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "no content",
+            "message": "No se encontraron usuarios",
+            "data": []
+          }
+        }
+      }
+    }
+  },
+  "get_user": {
+    status.HTTP_200_OK: {
+      "description": "Usuario encontrado",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "success",
+            "message": "Usuario encontrado",
+            "data": {
+              "username": "user1",
+              "id": 1,
+              "created_at": "2025-07-26T18:27:09"
+            }
+          }
+        }
+      }
+    },
+    status.HTTP_404_NOT_FOUND: {
+      "description": "Usuario no encontrado",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "error",
+            "message": "Usuario no encontrado",
+            "data": None
+          }
+        }
+      }
+    }
+  },
+  "register_user": {
+    status.HTTP_201_CREATED: {
+      "description": "Usuario registrado exitosamente",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "success",
+            "message": "Usuario registrado",
+            "data": {
+              "username": "user1",
+              "id": 1,
+              "created_at": "2025-07-26T18:27:09"
+            }
+          }
+        }
+      }
+    },
+    status.HTTP_409_CONFLICT: {
+      "description": "El usuario ya existe",
+      "content": {
+        "application/json": {
+          "example": {
+            "status": "error",
+            "message": "El usuario ya existe",
+            "data": None
+          }
+        }
+      }
+    }
+  }
+}
 # Endpoint para obtener todos los usuarios
-@router.get("/", response_model = APIResponse)
+@router.get(
+  "/",
+  summary="Get All Users",
+  description="Obtiene una lista de todos los usuarios registrados",
+  response_model = APIResponse,
+  responses=responses.get("get_users", {})  
+)
 def get_users(db: Session = Depends(get_db)):
     users = user_crud.get_all_users(db=db)
-    return APIResponse(status="success", message="Usuarios encontrados", data=users)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder(APIResponse(status="success", message="Usuarios encontrados", data=users))
+    ) if users else JSONResponse(
+        status_code=status.HTTP_204_NO_CONTENT,
+        content=jsonable_encoder(APIResponse(status="no content", message="No se encontraron usuarios"))
+    )
 # Endpoint para obtener un usuario por ID
-@router.get("/{user_id}", response_model = APIResponse)
-def get_user(user_id: int, db: Session = Depends(get_db)):
+@router.get(
+  "/{user_id}",
+  summary="Get User by ID",
+  description="Obtiene un usuario por su ID",
+  responses=responses.get("get_user", {})
+)
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     user = user_crud.get_user_by_id(user_id, db)
     if not user:
-        raise NotFoundException(name="Usuario")
-    return APIResponse(status="success", message="Usuario encontrado", data=user)
-# # Endpoint para registrar un nuevo usuario
-# @router.post("/register")
-# def register_user(data: UserCreate, db: Session = Depends(get_db), response_model = APIResponse):
-#     new_user = create_user(data, db)
-#     return APIResponse(status="success", message="Usuario registrado", data=new_user)
+        raise NotFoundException(message="Usuario no encontrado")
+    return JSONResponse(
+    status_code=200,
+        content=jsonable_encoder(APIResponse(status="success", message="Usuario encontrado", data=user))
+    )
+# Endpoint para registrar un nuevo usuario
+@router.post(
+  "/register",
+  summary="Register User",
+  description="Registra un nuevo usuario en el sistema",
+  responses=responses.get("register_user", {})
+)
+def register_user(data: UserCreate, db: Session = Depends(get_db)):
+    new_user = user_crud.create_user(data, db)
+    if not new_user:
+        raise ConflictException(message="El usuario ya existe")
+    return JSONResponse(
+        status_code=201,
+        content=jsonable_encoder(APIResponse(status="created", message="Usuario registrado", data=new_user))
+    )
