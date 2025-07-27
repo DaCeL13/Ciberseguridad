f805aa9752507f87c0f7ccb90bd294850d7687d8
Daniel Castellanos <danicage.13@outlook.com>
2025-07-27 11:39:26 -0500

    fixed custom and handlers exceptions

--- init-server/backend/app/exceptions/custom_exceptions.py
+++ init-server/backend/app/exceptions/custom_exceptions.py
@@ -1,8 +1,12 @@
 # Esta excepción se lanzará cuando no se encuentre un recurso
 class NotFoundException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-# Esta excepción se lanzará cuando no se pueda establecer una conexión con la base de datos        
+    def __init__(self, message: str = "Recurso no encontrado"):
+        self.message = message
+# Esta excepción se lanzará cuando no se pueda establecer una conexión con la base de datos
 class DatabaseConnectionException(Exception):
     def __init__(self, message: str = "Error al conectar con la base de datos"):
+        self.message = message
+
+class ConflictException(Exception):
+    def __init__(self, message: str = "Conflicto de datos"):
         self.message = message
--- init-server/backend/app/exceptions/handlers.py
+++ init-server/backend/app/exceptions/handlers.py
@@ -1,21 +1,31 @@
-from fastapi import Request
+from fastapi import Request, status
 from fastapi.responses import JSONResponse
-from app.exceptions.custom_exceptions import NotFoundException, DatabaseConnectionException
+from app.exceptions.custom_exceptions import NotFoundException, DatabaseConnectionException, ConflictException
 
 # Estructura para la excepción NotFoundException
 def not_found_exception_handler(request: Request, exc: NotFoundException):
     return JSONResponse(
-        status_code=404,
+        status_code=status.HTTP_404_NOT_FOUND,
         content={
             "status": "error",
-            "message": f"{exc.name} no encontrado",
+            "message": exc.message,
             "data": None
         }
     )
 # Estructura para la excepción DatabaseConnectionException
 def db_connection_exception_handler(request: Request, exc: DatabaseConnectionException):
     return JSONResponse(
-        status_code=500,
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            "status": "error",
+            "message": exc.message,
+            "data": None
+        }
+    )
+# Estructura para la excepción ConflictException
+def conflict_exception_handler(request: Request, exc: ConflictException):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
         content={
             "status": "error",
             "message": exc.message,
